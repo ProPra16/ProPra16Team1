6659324b9f256469f4ab221c9bdecf349c1a2655
edsch103 <eduard.schleicher@hhu.de>
2016-06-25 19:26:29 +0200

    first

--- GUI.java
+++ GUI.java
@@ -14,8 +14,12 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
 
 public class GUI extends Application{
+
+private Stage mainMenu;
+private int exc_auswahl = -1;        //Falls keine Wahl getroffen wurde -1
 
 public static void main(String[] args) {
 launch(args);
@@ -44,14 +48,15 @@ launch(args);
  mainStage.setScene(sc_mainmenu);
  mainStage.setTitle("Hauptemü");
  mainStage.show();
+ mainMenu = mainStage;
 
   bt_training.setOnAction(new EventHandler<ActionEvent>() {
   @Override public void handle(ActionEvent e) {
     mainStage.hide();
     Stage st_exc_selection = new Stage();
-    Loader non_static_af = new Loader();
+    Loader non_static_af = new Loader();          //dazu da um static Referenzprobleme zu behandeln
 
-   non_static_af.read_exc();
+    non_static_af.read_exc();
 
    ArrayList<String> Aufgaben_Namen = new ArrayList<String>();
     for(int i=0;i<non_static_af.Aufgaben_Verwaltung.size();i++){
@@ -71,7 +76,18 @@ launch(args);
    txF_beschr.setPrefHeight(100);
    txF_beschr.setEditable(false);
    txF_beschr. setWrapText(true); 
+
+   Button bt_back = new Button();
+   bt_back.setText("Zurück");
+
+   Button bt_select = new Button();
+   bt_select.setText("Übung beginnen");
+
+   Label noSelection = new Label("Bitte wählen Sie eine Übung aus!");
+   noSelection.setVisible(false);
  
+   Label warning = new Label("WARNUNG:Bestehende Übung wird gelöscht!");
+
    GridPane exc_layout = new GridPane();
    exc_layout.setAlignment(Pos.CENTER);
    exc_layout.setHgap(10);
@@ -79,6 +95,10 @@ launch(args);
    exc_layout.add(exc_comboBox,1,0);
    exc_layout.add(tx_auswahl,0,0);
    exc_layout.add(txF_beschr,0,4);
+   exc_layout.add(bt_back,1,30);
+   exc_layout.add(bt_select,0,5);
+   exc_layout.add(warning,0,6);
+   exc_layout.add(noSelection,0,7);
 
    Scene sc_choose = new Scene(exc_layout,500,500);
    st_exc_selection.setScene(sc_choose);  
@@ -88,13 +108,31 @@ launch(args);
 
    exc_comboBox.valueProperty().addListener(new ChangeListener<String>() {
         @Override public void changed(ObservableValue ov, String old_value, String new_value) {
-         int new_Beschreibung = Aufgaben_Namen.indexOf(new_value);
-         String desc = non_static_af.Aufgaben_Verwaltung.get(new_Beschreibung).getBeschreibung();
+         exc_auswahl = Aufgaben_Namen.indexOf(new_value);
+         String desc = non_static_af.Aufgaben_Verwaltung.get(exc_auswahl).getBeschreibung();
          txF_beschr.setText(desc);
+         noSelection.setVisible(false);
         }    
     });
-   }});
 
+   bt_back.setOnAction(new EventHandler<ActionEvent>() {
+   @Override public void handle(ActionEvent e) {
+   mainMenu.show();
+   st_exc_selection.hide();
+    }});
+
+   bt_select.setOnAction(new EventHandler<ActionEvent>() {
+   @Override public void handle(ActionEvent e) {
+    if(exc_auswahl == -1){
+     noSelection.setVisible(true);
+     return;
+     }
+    st_exc_selection.hide();
+    Stage editor = new Stage();
+
+
+    }});
+   }});
    
 
 
